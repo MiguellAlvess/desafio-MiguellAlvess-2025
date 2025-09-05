9ae62fea31ab7da46130384f1a06dbbdd00ad6dc
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-05 18:23:39 -0300

    feat: adiciona teste para validação de animais duplicados e corrige importação de ErroAnimalInvalido

--- src/application/validador-entrada.test.js
+++ src/application/validador-entrada.test.js
@@ -1,6 +1,7 @@
 import { RepositorioAnimaisEmMemoria } from "../../src/infra/repositorio-animais-memoria.js";
 import { ValidadorEntradas } from "./validator-entradas.js";
 import { ErroBrinquedoInvalido } from "./errors/erro-brinquedo-invalido.js";
+import { ErroAnimalInvalido } from "./errors/erro-animal-invalido.js";
 
 describe("ValidadorEntradas", () => {
   test("Deve lançar erro quando brinquedo não está na lista de válidos", () => {
@@ -9,9 +10,17 @@ describe("ValidadorEntradas", () => {
       ErroBrinquedoInvalido
     );
   });
+
   test("Deve lançar erro quando animal não existe no repositório", () => {
     const validador = new ValidadorEntradas(new RepositorioAnimaisEmMemoria());
     expect(() => validador.validarAnimais(["Lulu"])).toThrow(
+      ErroAnimalInvalido
+    );
+  });
+
+  test("Deve lançar erro quando há animais duplicados", () => {
+    const validador = new ValidadorEntradas(new RepositorioAnimaisEmMemoria());
+    expect(() => validador.validarAnimais(["Rex", "rex"])).toThrow(
       ErroAnimalInvalido
     );
   });
